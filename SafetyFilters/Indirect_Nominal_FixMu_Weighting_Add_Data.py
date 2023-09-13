d0ef2f5dc967a31b8af84ad6046fd4060d8845ad
Yujun Huang <yujhuang@ethz.ch>
2023-09-13 21:47:16 +0200

    Better performance with well-tuned HP

--- SafetyFilters/Indirect_Nominal_FixMu_Weighting_Add_Data.py
+++ SafetyFilters/Indirect_Nominal_FixMu_Weighting_Add_Data.py
@@ -238,7 +238,9 @@ class IndirectNominalFixMuWeightingAddDataFilter(DDSafetyFilter):
                 y = y_history[i*self._p:(i+1)*self._p]
                 n = np.matrix(np.zeros(y.shape))
                 self._io_data_list[-1].add_point(u, y, n)
-                is_empty = not self._io_data_list[0].remove_last_point()
+                is_empty = False
+                if self._io_data_list[-1].length > self._L+self._lag:
+                    is_empty = not self._io_data_list[0].remove_last_point()
             if is_empty: # if a dataset is empty, remove it
                 self._io_data_list.pop(0)
                 self._first_io_data_poped = True
